0cf4b5545e3585fbd0dc760723044cff882cbdca
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2016-03-23 03:44:01 +0530

    Adds updatePopularMovies method and executes it on start

--- app/src/main/java/com/benzene/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/benzene/popularmovies/MainActivityFragment.java
@@ -2,16 +2,33 @@ package com.benzene.popularmovies;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.benzene.popularmovies.api.TheMovieDBAPI;
+import com.benzene.popularmovies.model.PopularMovies;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements retrofit2.Callback<PopularMovies> {
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
 
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updatePopularMovies();
     }
 
     @Override
@@ -19,5 +36,31 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
+    }
+
+    private void updatePopularMovies() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.themoviedb.org")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        TheMovieDBAPI theMovieDBAPI = retrofit.create(TheMovieDBAPI.class);
+        Call<PopularMovies> call = theMovieDBAPI.getPopular(BuildConfig.THE_MOVIEDB_API_KEY);
+
+        call.enqueue(this);
+    }
+
+    @Override
+    public void onResponse(Call<PopularMovies> call, Response<PopularMovies> response) {
+        PopularMovies popularMovies = response.body();
+        Log.d(LOG_TAG, popularMovies.getTotalPages().toString());
+    }
+
+    @Override
+    public void onFailure(Call<PopularMovies> call, Throwable t) {
+        Toast.makeText(
+                getActivity(),
+                getString(R.string.error_data_fetching),
+                Toast.LENGTH_SHORT).show();
     }
 }
